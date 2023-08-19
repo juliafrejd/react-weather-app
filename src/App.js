a0738270b7ffa5526a2ef1da2f66d6921d28dcf8
juliafrejd <118212457+juliafrejd@users.noreply.github.com>
2023-08-19 10:08:18 +0100

    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      React Weather App
+      <footer>
+        This project was coded by Julia Frejd and is open sourced{" "}
+        <a
+          href="https://github.com/juliafrejd/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
