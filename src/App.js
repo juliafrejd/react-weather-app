ed5b8aa4740791d0b7f199c530714c3d01ecae45
juliafrejd <118212457+juliafrejd@users.noreply.github.com>
2023-08-19 10:55:22 +0100

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      React Weather App
-      <footer>
-        This project was coded by Julia Frejd and is open sourced{" "}
-        <a
-          href="https://github.com/juliafrejd/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        React Weather App
+        <footer>
+          This project was coded by Julia Frejd and is open sourced{" "}
+          <a
+            href="https://github.com/juliafrejd/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
