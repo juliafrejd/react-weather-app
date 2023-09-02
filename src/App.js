3a863bd8a8578e5f84c8c632419d15acb2ff71fe
juliafrejd <118212457+juliafrejd@users.noreply.github.com>
2023-09-02 14:00:36 +0100

    Looping array for 5 day forecast

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Lisbon" />
+        <Weather defaultCity="Tokyo" />
         <footer>
           This project was coded by Julia Frejd and is open sourced{" "}
           <a
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -9,9 +9,16 @@ export default function WeatherForecastDay(props) {
     let temperature = Math.round(props.data.temperature.minimum);
     return `${temperature}`;
   }
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
   return (
     <div>
-      <div className="WeatherForecast-day">{props.data.time}</div>
+      <div className="WeatherForecast-day">{day()}</div>
       <div className="WeatherForecast-icon">
         <img
           src={props.data.condition.icon_url}
